994bf509433fabea6fda8a8c604415eafca52be6
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2022-03-17 14:54:03 +0400

    Готово

--- 2_lab_14.py
+++ 2_lab_14.py
@@ -42,11 +42,63 @@ print(C)
 print("_______________________________________________________________________________________________")
 
 """пункт 3"""
-print("Создать произвольную нижнюю унитреугольную матрицу А 7 порядка,\n"
+print("Создать произвольную нижняя унитреугольную матрицу А 7 порядка,\n"
       "вектор B произвольный. Решить систему AX = B")
 
+A = [[1,0,0,0,0,0,0],
+     [2,1,0,0,0,0,0],
+     [3,1,1,0,0,0,0],
+     [1,0,2,1,0,0,0],
+     [0,2,0,4,1,0,0],
+     [1,0,3,0,6,1,0],
+     [0,4,0,5,0,6,1]]
+
+В = [[0],[1],[2],[1],[0],[0],[1]]
+X = np.zeros((7,1), int)
+for i in range(7,1):
+    X[i] = B[i] - np.dot(A[i, :i], X[ :i])
+print(X)
 
 
+print("_______________________________________________________________________________________________")
 
+print("Решить систему, используя LU разложение ")
+
+a = np.array([[3.8,14.2,6.3,-15.5],
+             [8.3,-6.6,5.8,12.2],
+             [6.4,-8.5,-4.3,8.8],
+             [17.1,-8.3,14.4,-7.2]])
+
+b = np.array([[2.8],[-4.7],[7.7],[13.5]])
+
+def decompose_to_LU(a):
+    lu_matrix = np.matrix(np.zeros([a.shape[0], a.shape[1]]))
+    n = a.shape[0]
+
+    for k in range(n):
+        for j in range(k, n):
+            lu_matrix[k, j] = a[k, j] - lu_matrix[k, :k] * lu_matrix[:k, j]
+        print(lu_matrix)
+        for i in range(k + 1, n):
+            lu_matrix[i, k] = (a[i, k] - lu_matrix[i, : k] * lu_matrix[: k, k]) / lu_matrix[k, k]
+        print(lu_matrix)
+    return lu_matrix
+
+lu_matrix = decompose_to_LU(a)
+
+def solve_LU(lu_matrix, b):
+    y = np.matrix(np.zeros([lu_matrix.shape[0], 1]))
+
+    for i in range(y.shape[0]):
+        y[i, 0] = b[i, 0] - lu_matrix[i, :i] * y[:i]
+
+    x = np.matrix(np.zeros([lu_matrix.shape[0], 1]))
+    for i in range(1, x.shape[0] + 1):
+        x[-i, 0] = (y[-i] - lu_matrix[-i, -i:] * x[-i:, 0] )/ lu_matrix[-i, -i]
+
+    return x
+
+res = solve_LU(lu_matrix, b)
+print(res)
 
 
